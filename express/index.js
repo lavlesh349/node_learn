9ed9a3688e5d3bdf4253158dd2d5744d48186fd7
Your Name <you@example.com>
2025-08-25 22:03:39 +0530

    rest-project without database

--- express/index.js
+++ express/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 
 const app = express();//app is a handler function 
-app.get("/", (req, res) => {
-    return res.send("Hello from home page");
+
+app.use('/', (req, res, next) => {
+    console.log(1);
+    res.send("hello from root");
+    
+    next();
 })
+// app.get("/", (req, res) => {
+//     return res.send("Hello from home page");
+// })
 
 app.get("/about", (req, res) => {
     return res.send("Hello from About Page" + "hey" + req.query.name);
